7fbb3698d6b80a9f2b5422e3c1faf4a0ac2a2286
HandaR <midrau18@gamil.com>
2020-12-29 10:06:06 +0900

    ログイン機能を追加しました

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get  '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+
   root to: 'ballpark_logs#index'
   resources :ballpark_logs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
