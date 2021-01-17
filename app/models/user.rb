7d64dc5a64e3cc64efa55ed06a1b49f856edddb7
HandaR <midrau18@gamil.com>
2021-01-17 22:10:50 +0900

    ユーザーアイコン画像を設定して、詳細画面と投稿一覧画面に表示できるようにしました

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,8 @@ class User < ApplicationRecord
     has_secure_password
     validates :email, uniqueness: true
 
+    has_one_attached :image
+
     has_many :ballpark_logs
     has_many :likes
     has_many :like_ballpark_logs, through: :likes, source: :ballpark_log
