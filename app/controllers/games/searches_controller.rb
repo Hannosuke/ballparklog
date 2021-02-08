5ff4b2c51c050c98103ab6a2b1baa8c094a23ffa
HandaR <midrau18@gamil.com>
2021-02-08 21:26:17 +0900

    投稿フォームで、選択した日付の試合データを取得できるようにした

--- app/controllers/games/searches_controller.rb
+++ app/controllers/games/searches_controller.rb
@@ -1,5 +1,11 @@
 class Games::SearchesController < ApplicationController
     def index
+        @games = Game.where(date: "%#{params[:date]}%")
         binding.pry
+
+        respond_to do |format|
+            format.html { redirect_to :root }
+            format.json { render json: @games }
+        end
     end
-  end
+end
