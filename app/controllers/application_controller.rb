44c3a345df201ff26ef4b80e57abc5ae6682a7bc
HandaR <midrau18@gamil.com>
2021-02-15 11:02:50 +0900

    ログイン中のログイン・新規登録画面へのアクセス制限を追加しました

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,15 @@ class ApplicationController < ActionController::Base
 
     def authenticate_user
         if current_user == nil
-          flash[:notice] = "ログインが必要です"
-          redirect_to("/login")
+            flash[:notice] = "ログインが必要です"
+            redirect_to("/login")
+        end
+    end
+
+    def forbid_login_user
+        if current_user
+            flash[:notice] = "すでにログインしています"
+            redirect_to("/")
         end
     end
 end
