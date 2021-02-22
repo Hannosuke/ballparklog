1c0d276b06442a7c6f1a127c446a9ec2ef48ed3a
HandaR <midrau18@gamil.com>
2021-02-22 15:44:41 +0900

    新規ユーザー登録後、自動的にログインがなされるようにしました

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:notice] = "ユーザー登録が完了しました"
       redirect_to("/")
     else
