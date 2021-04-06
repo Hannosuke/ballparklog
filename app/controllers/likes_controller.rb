a70254a70613c19cbcab4601470efaa36c4d99bb
HandaR <midrau18@gamil.com>
2021-04-06 15:07:48 +0900

    いいねボタンをrender partialによる切り替え表示に書き換え

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,21 +3,21 @@
 class LikesController < ApplicationController
   before_action :authenticate_user
 
-  def create
+  def create 
+    @ballpark_log = BallparkLog.find(params[:ballpark_log_id])
     @like = Like.new(
       user_id: current_user.id,
-      ballpark_log_id: params[:ballpark_log_id]
+      ballpark_log_id: @ballpark_log.id
     )
     @like.save
-    render json: @like
   end
 
   def destroy
+    @ballpark_log = BallparkLog.find(params[:ballpark_log_id])
     @like = Like.find_by(
       user_id: current_user.id,
-      ballpark_log_id: params[:ballpark_log_id]
+      ballpark_log_id: @ballpark_log.id
     )
     @like.destroy
-    redirect_to("/")
   end
 end
